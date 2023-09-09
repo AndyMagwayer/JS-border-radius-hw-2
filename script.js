23902085c461abde46b45c5562b4abb8179cb0df
AndyMagwayer <andymagwayer@gmial.com>
2023-09-09 16:18:49 +0500

    fix: fixed bags

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 const box = document.getElementById('box');
-const radiusInput = document.getElementById('radius-input');
-const sizeInput = document.getElementById('size-input');
-const colorInput = document.getElementById('color-input');
+const topLeftInput = document.getElementById('top-left-input');
+const topRightInput = document.getElementById('top-right-input');
+const bottomRightInput = document.getElementById('bottom-right-input');
+const bottomLeftInput = document.getElementById('bottom-left-input');
 const cssCode = document.getElementById('css-code');
 
+function updateBox() {
+  const topLeft = topLeftInput.value;
+  const topRight = topRightInput.value;
+  const bottomRight = bottomRightInput.value;
+  const bottomLeft = bottomLeftInput.value;
+  
+  box.style.borderTopLeftRadius = `${topLeft}px`;
+  box.style.borderTopRightRadius = `${topRight}px`;
+  box.style.borderBottomRightRadius = `${bottomRight}px`;
+  box.style.borderBottomLeftRadius = `${bottomLeft}px`;
+  
+  updateCSSCode();
+}
 
-radiusInput.addEventListener('input', () => {
-    const radius = radiusInput.value;
-    box.style.borderRadius = `${radius}px`;
-    updateCSSCode();
-});
+function updateCSSCode() {
+  const topLeft = topLeftInput.value;
+  const topRight = topRightInput.value;
+  const bottomRight = bottomRightInput.value;
+  const bottomLeft = bottomLeftInput.value;
+  
+  const css = `#box {
+    border-top-left-radius: ${topLeft}px;
+    border-top-right-radius: ${topRight}px;
+    border-bottom-right-radius: ${bottomRight}px;
+    border-bottom-left-radius: ${bottomLeft}px;
+  }`;
+  
+  cssCode.textContent = css;
+}
 
-
-sizeInput.addEventListener('input', () => {
-    const color = colorInput.value;
-    box.style.backgroundColor = color; 
-    updateCSSCode();
-});
-
-
-colorInput.addEventListener('input' , () => {
-    const color = color.value;
-    box.style.backgroundColor = color;
-    updateCSSCode();
-});
-
+topLeftInput.addEventListener('input', updateBox);
+topRightInput.addEventListener('input', updateBox);
+bottomRightInput.addEventListener('input', updateBox);
+bottomLeftInput.addEventListener('input', updateBox);
